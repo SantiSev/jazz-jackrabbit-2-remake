a404107d6bd1b5f785ec7e24206b0fe1f8371eaf
Agustin Barbalase <abarbalase@fi.uba.ar>
2024-05-24 08:47:12 -0300

    Missing change `ServerProtocol::recv_message()` in `server_main.cpp`

--- server/server_main.cpp
+++ server/server_main.cpp
@@ -15,7 +15,7 @@ int main(int argc, char* argv[]) {
         ServerProtocol protocol(acceptor.accept());
 
         while (!protocol.is_closed()) {
-            std::string msg = protocol.recv_msg();
+            std::string msg = protocol.recv_message();
             if (protocol.is_closed())
                 break;
             std::cout << msg << std::endl;
