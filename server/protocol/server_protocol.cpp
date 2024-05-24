e225f90c93300b5844e752e08f5b29938a8076aa
Agustin Barbalase <abarbalase@fi.uba.ar>
2024-05-24 08:48:32 -0300

    Changing header to `uint16_t` and Adding more events

--- server/protocol/server_protocol.cpp
+++ server/protocol/server_protocol.cpp
@@ -1,20 +1,31 @@
 #include "./server_protocol.h"
 
+#include <arpa/inet.h>
+
 ServerProtocol::ServerProtocol(Socket&& skt): client(std::move(skt)), was_closed(false) {}
 
 const std::string ServerProtocol::recv_message() {
-    uint8_t header;
+    uint16_t header;
 
     client.recvall(&header, sizeof(header), &was_closed);
+    header = ntohs(header);
     if (was_closed)
         return "";
 
-    if (header == 0x00)
-        return "CONNECTION_EVENT";
-    else if (header == 0x01)
-        return "IN_GAME_EVENT";
-    else if (header == 0x02)
-        return "MENU_EVENT";
+    if (header == 0x0000)
+        return "CLOSE_CONNECTION";
+    else if (header == 0x0101)
+        return "RECV_COMMAND";
+    else if (header == 0x0102)
+        return "RECV_CHEAT_COMMAND";
+    else if (header == 0x0103)
+        return "RECV_UNJOIN_MATCH";
+    else if (header == 0x0201)
+        return "RECV_CREATE_GAME";
+    else if (header == 0x0202)
+        return "RECV_GAME_CREATED";
+    else if (header == 0x0203)
+        return "RECV_JOIN_MATCH";
 
     return "";
 }
