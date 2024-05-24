4de598d813122bd040d57868a28d6a1d64ac03ea
Agustin Barbalase <abarbalase@fi.uba.ar>
2024-05-24 10:12:21 -0300

    Chaging `map` to switch and deleting `ServerProtocol::deserialize_header()`

--- server/protocol/server_protocol.cpp
+++ server/protocol/server_protocol.cpp
@@ -2,17 +2,15 @@
 
 #include <arpa/inet.h>
 
-ServerProtocol::ServerProtocol(Socket&& skt): client(std::move(skt)), was_closed(false) {
-    events[0x0000] = "CLOSE_CONNECTION";
-    events[0x0101] = "RECV_COMMAND";
-    events[0x0102] = "RECV_CHEAT_COMMAND";
-    events[0x0103] = "RECV_UNJOIN_MATCH";
-    events[0x0201] = "RECV_CREATE_GAME";
-    events[0x0202] = "RECV_GAME_CREATED";
-    events[0x0203] = "RECV_JOIN_MATCH";
-}
+#define CLOSE_CONNECTION 0x0000
+#define RECV_COMMAND 0x0101
+#define RECV_CHEAT_COMMAND 0x0102
+#define RECV_UNJOIN_MATCH 0x0103
+#define RECV_CREATE_GAME 0x0201
+#define RECV_GAME_CREATED 0x0202
+#define RECV_JOIN_MATCH 0x0203
 
-const std::string ServerProtocol::deserialize_header(uint16_t header) { return events[header]; }
+ServerProtocol::ServerProtocol(Socket&& skt): client(std::move(skt)), was_closed(false) {}
 
 const std::string ServerProtocol::recv_message() {
     uint16_t header;
@@ -22,7 +20,20 @@ const std::string ServerProtocol::recv_message() {
     if (was_closed)
         return "";
 
-    return deserialize_header(header);
+    switch (header) {
+        case CLOSE_CONNECTION:
+            break;
+        case RECV_COMMAND:
+            break;
+        case RECV_UNJOIN_MATCH:
+            break;
+        case RECV_CREATE_GAME:
+            break;
+        case RECV_GAME_CREATED:
+            break;
+        case RECV_JOIN_MATCH:
+            break;
+    }
 }
 
 bool ServerProtocol::is_closed() const { return was_closed; }
