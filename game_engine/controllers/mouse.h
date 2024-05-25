974f6f09d57af34bdfc945186da4629bc559a593
maxogod <maxi18wars@gmail.com>
2024-05-25 20:06:52 -0300

    Create Controller abstract class

--- game_engine/controllers/mouse.h
+++ game_engine/controllers/mouse.h
@@ -4,20 +4,24 @@
 #include <list>
 
 #include <SDL2/SDL.h>
+#include "controller.h"
 
 typedef void (*on_click_signal_handler)(SDL_Point);
 
-class Mouse {
+class Mouse : public Controller {
 private:
     std::list<on_click_signal_handler> signal_handlers;
 
 public:
     SDL_Point cursor;
+
     explicit Mouse(int x, int y);
-    void update(const SDL_Event& event);
+
+    void update(const SDL_Event& event) override;
     void add_on_click_signal_handler(on_click_signal_handler handler);
     void remove_on_click_signal_handler(on_click_signal_handler handler);
-    ~Mouse();
+    
+    ~Mouse() override;
 };
 
 
