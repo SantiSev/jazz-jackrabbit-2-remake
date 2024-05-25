98b5032b17ca63c240a3b2f91e056a070ca938ed
Agustin Barbalase <abarbalase@fi.uba.ar>
2024-05-25 17:34:17 -0300

    Adding functions to `ClientProtocol`
    - `ClientProtocol::recv_one_byte()`: Receives one byte
    - `ClientProtocol::recv_two_bytes()`: Receives two bytes
    - `ClientProtocol::recv_string()`: Receives a string of one byte length

--- client/protocol/client_protocol.cpp
+++ client/protocol/client_protocol.cpp
@@ -1,5 +1,9 @@
 #include "./client_protocol.h"
 
+#include <vector>
+
+#include <arpa/inet.h>
+
 #define CLOSE_CONNECTION 0x0000
 
 #define SEND_GAME_STATE 0x0100
@@ -15,5 +19,39 @@
 
 ClientProtocol::ClientProtocol(const std::string& hostname, const std::string& servname):
         server(hostname.c_str(), servname.c_str()), was_closed(false) {}
+
+const uint8_t ClientProtocol::recv_one_byte() {
+    uint8_t one_byte;
+
+    server.recvall(&one_byte, sizeof(one_byte), &was_closed);
+    if (was_closed)
+        return CLOSE_CONNECTION;
+
+    return one_byte;
+}
+
+const uint16_t ClientProtocol::recv_two_bytes() {
+    uint16_t two_bytes;
+
+    server.recvall(&two_bytes, sizeof(two_bytes), &was_closed);
+    if (was_closed)
+        return CLOSE_CONNECTION;
+
+    return ntohs(two_bytes);
+}
+
+const std::string ClientProtocol::recv_string() {
+    uint8_t length;
+
+    server.recvall(&length, sizeof(length), &was_closed);
+    length = ntohs(length);
+
+    std::vector<char> buf(length);
+    server.recvall(buf.data(), length, &was_closed);
+
+    std::string result(buf.begin(), buf.end());
+
+    return result;
+}
 
 ClientProtocol::~ClientProtocol() {}
--- client/protocol/client_protocol.h
+++ client/protocol/client_protocol.h
@@ -9,6 +9,10 @@ private:
     Socket server;
     bool was_closed;
 
+    const uint8_t recv_one_byte();
+    const uint16_t recv_two_bytes();
+    const std::string recv_string();
+
 public:
     ClientProtocol(const std::string& hostname, const std::string& servname);
 
