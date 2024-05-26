f9a37a8ad59e98dbee619c2cf9b3291ca5f26f1b
Agustin Barbalase <abarbalase@fi.uba.ar>
2024-05-26 17:39:59 -0300

    Made the `ClientProtocol` class inheritable from `CommonProtocol`

--- client/protocol/client_protocol.cpp
+++ client/protocol/client_protocol.cpp
@@ -18,103 +18,69 @@
 #define RECV_JOIN_MATCH 0x0203
 
 ClientProtocol::ClientProtocol(const std::string& hostname, const std::string& servname):
-        server(hostname.c_str(), servname.c_str()), was_closed(false) {}
-
-const uint8_t ClientProtocol::recv_one_byte() {
-    uint8_t one_byte;
-
-    server.recvall(&one_byte, sizeof(one_byte), &was_closed);
-    if (was_closed)
-        return CLOSE_CONNECTION;
-
-    return one_byte;
-}
-
-const uint16_t ClientProtocol::recv_two_bytes() {
-    uint16_t two_bytes;
-
-    server.recvall(&two_bytes, sizeof(two_bytes), &was_closed);
-    if (was_closed)
-        return CLOSE_CONNECTION;
-
-    return ntohs(two_bytes);
-}
-
-const std::string ClientProtocol::recv_string() {
-    uint8_t length;
-
-    server.recvall(&length, sizeof(length), &was_closed);
-    length = ntohs(length);
-
-    std::vector<char> buf(length);
-    server.recvall(buf.data(), length, &was_closed);
-
-    std::string result(buf.begin(), buf.end());
-
-    return result;
-}
+        CommonProtocol(hostname, servname) {}
 
 void ClientProtocol::send_command(uint16_t id_player, uint8_t id_command) {
     uint16_t header = htons(RECV_COMMAND);
-    server.sendall(&header, sizeof(header), &was_closed);
+    skt.sendall(&header, sizeof(header), &was_closed);
     if (was_closed)
         return;
 
     id_player = htons(id_player);
-    server.sendall(&id_player, sizeof(id_player), &was_closed);
+    skt.sendall(&id_player, sizeof(id_player), &was_closed);
     if (was_closed)
         return;
 
-    server.sendall(&id_command, sizeof(id_command), &was_closed);
+    skt.sendall(&id_command, sizeof(id_command), &was_closed);
     if (was_closed)
         return;
 }
 
 void ClientProtocol::send_cheat_command(uint16_t id_player, uint8_t id_cheat_command) {
     uint16_t header = htons(RECV_CHEAT_COMMAND);
-    server.sendall(&header, sizeof(header), &was_closed);
+    skt.sendall(&header, sizeof(header), &was_closed);
     if (was_closed)
         return;
 
     id_player = htons(id_player);
-    server.sendall(&id_player, sizeof(id_player), &was_closed);
+    skt.sendall(&id_player, sizeof(id_player), &was_closed);
     if (was_closed)
         return;
 
-    server.sendall(&id_cheat_command, sizeof(id_cheat_command), &was_closed);
+    skt.sendall(&id_cheat_command, sizeof(id_cheat_command), &was_closed);
     if (was_closed)
         return;
 }
 
 void ClientProtocol::send_leave_match(uint16_t id_player) {
     uint16_t header = htons(RECV_LEAVE_MATCH);
-    server.sendall(&header, sizeof(header), &was_closed);
+    skt.sendall(&header, sizeof(header), &was_closed);
     if (was_closed)
         return;
 
     id_player = htons(id_player);
-    server.sendall(&id_player, sizeof(id_player), &was_closed);
+    skt.sendall(&id_player, sizeof(id_player), &was_closed);
     if (was_closed)
         return;
 }
 
 void ClientProtocol::send_create_game(uint16_t id_player, std::string& match_name) {
     uint16_t header = htons(RECV_CREATE_GAME);
-    server.sendall(&header, sizeof(header), &was_closed);
+    skt.sendall(&header, sizeof(header), &was_closed);
     if (was_closed)
         return;
 
     id_player = htons(id_player);
-    server.sendall(&id_player, sizeof(id_player), &was_closed);
+    skt.sendall(&id_player, sizeof(id_player), &was_closed);
     if (was_closed)
         return;
 
     uint8_t length = match_name.length();
-    server.sendall(&length, sizeof(length), &was_closed);
+    skt.sendall(&length, sizeof(length), &was_closed);
     if (was_closed)
         return;
 
-    server.sendall(match_name.data(), length, &was_closed);
+    skt.sendall(match_name.data(), length, &was_closed);
     if (was_closed)
         return;
 }
@@ -122,21 +88,21 @@ void ClientProtocol::send_create_game(uint16_t id_player, std::string& match_nam
 void ClientProtocol::send_join_match(uint16_t id_player, uint16_t id_match,
                                      uint8_t player_character) {
     uint16_t header = htons(RECV_JOIN_MATCH);
-    server.sendall(&header, sizeof(header), &was_closed);
+    skt.sendall(&header, sizeof(header), &was_closed);
     if (was_closed)
         return;
 
     id_player = htons(id_player);
-    server.sendall(&id_player, sizeof(id_player), &was_closed);
+    skt.sendall(&id_player, sizeof(id_player), &was_closed);
     if (was_closed)
         return;
 
     id_match = htons(id_match);
-    server.sendall(&id_match, sizeof(id_match), &was_closed);
+    skt.sendall(&id_match, sizeof(id_match), &was_closed);
     if (was_closed)
         return;
 
-    server.sendall(&player_character, sizeof(player_character), &was_closed);
+    skt.sendall(&player_character, sizeof(player_character), &was_closed);
     if (was_closed)
         return;
 }
--- client/protocol/client_protocol.h
+++ client/protocol/client_protocol.h
@@ -2,6 +2,7 @@
 #include <string>
 
 #include "../../common/common_socket.h"
+#include "../../common/protocol/common_protocol.h"
 #include "./messages/connection_events/close_connection.h"
 #include "./messages/in_game_events/send_finish_match.h"
 #include "./messages/in_game_events/send_game_state.h"
@@ -9,24 +10,17 @@
 #include "./messages/menu_events/send_active_games.h"
 #include "./messages/menu_events/send_game_created.h"
 
-class ClientProtocol {
+class ClientProtocol: public CommonProtocol {
 private:
-    Socket server;
-    bool was_closed;
-    
-    const uint8_t recv_one_byte();  // unusedPrivateFunction
-    const uint16_t recv_two_bytes();
-    const std::string recv_string();  // unusedPrivateFunction
-
     std::unique_ptr<SendFinishMatchMessage> recv_finish_match();
     std::unique_ptr<SendGameStateMessage> recv_game_state();
 
     std::unique_ptr<SendActiveGamesMessage> recv_active_games();
     std::unique_ptr<SendGameCreatedMessage> recv_game_created();
-  
+
 public:
     ClientProtocol(const std::string& hostname, const std::string& servname);
-  
+
     void send_command(uint16_t id_player, uint8_t id_command);
 
     void send_cheat_command(uint16_t id_player, uint8_t id_cheat_command);
