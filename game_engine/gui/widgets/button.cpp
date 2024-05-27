24430fcaf1757b0582fc39f1904aebde551eb4ff
maxogod <maxi18wars@gmail.com>
2024-05-27 11:16:52 -0300

    Cambios en Button 	- revivo los cambios perdidos por el merge de mierduki 	- tiene label como componente y llama a sus metodos on_click y is_hovered

--- game_engine/gui/widgets/button.cpp
+++ game_engine/gui/widgets/button.cpp
@@ -1,23 +1,29 @@
 #include "button.h"
 
-Button::Button(SDL_Rect rect, SDL_Color color, SDL_Color hover_color):
-        rect(rect), color(color), hover_color(hover_color), is_hovered(false) {}
+Button::Button(const SDL_Rect& rect, const SDL_Color& color, const SDL_Color& hover_color,
+               Label& label)
+    : rect(rect), color(color), hover_color(hover_color), is_hovered_m(false), label(label) {}
 
-void Button::update(Mouse& mouse) {
-    SDL_Point mousePoint = {mouse.cursor.x, mouse.cursor.y};
-    is_hovered = SDL_PointInRect(&mousePoint, &this->rect);
+void Button::on_click() {
+    std::cout << "Button was clicked." << std::endl;
+    label.on_click();
 }
 
-void Button::on_click() { std::cout << "I was clicked" << std::endl; }
+void Button::is_hovered(bool hovered) {
+    is_hovered_m = hovered;
+    label.is_hovered(hovered);
+}
 
 void Button::draw(SDL_Renderer* renderer) {
-    if (is_hovered) {
+    if (is_hovered_m) {
         SDL_SetRenderDrawColor(renderer, hover_color.r, hover_color.g, hover_color.b,
                                hover_color.a);
     } else {
         SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
     }
     SDL_RenderFillRect(renderer, &rect);
+
+    label.draw(renderer);
 }
 
 bool Button::is_intersecting(SDL_Point& point) { return SDL_PointInRect(&point, &this->rect); }
