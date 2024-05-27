b508bb65b95924e95dd47b982da4e1a66aab084d
maxogod <maxi18wars@gmail.com>
2024-05-27 11:19:23 -0300

    Agrego prueba de boton y label y mouse 	- recupero lo borrado 	- prueba para ver como funciona

--- tests/graphics/graphics_test.cpp
+++ tests/graphics/graphics_test.cpp
@@ -2,19 +2,26 @@
 #include <iostream>
 
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
 
 #include "../../game_engine/controllers/mouse.h"
 #include "../../game_engine/gui/widgets/button.h"
+#include "../../game_engine/gui/widgets/label.h"
 
 int main() {
     SDL_Window* window = nullptr;
     SDL_Renderer* renderer = nullptr;
     SDL_Event event;
     SDL_Init(SDL_INIT_EVERYTHING);
+    TTF_Init();
+    TTF_Font* font = TTF_OpenFont("/home/maxo/Desktop/taller/game_engine/fonts/atus.ttf", 15);
+
     bool running = true;
 
     Mouse mouse(0, 0);
-    Button button({0, 0, 100, 100}, {255, 255, 255, 255}, {0, 0, 0, 0});
+    Label label({300, 200, 100, 100}, {0, 0, 0}, {255, 255, 255}, "Hello, World!", font);
+    Button button({300, 200, 100, 100}, {255, 255, 255}, {0, 0, 0}, label);
+
     mouse.add_on_click_signal_obj(&button);
 
     SDL_CreateWindowAndRenderer(800, 600, 0, &window, &renderer);
@@ -24,7 +31,6 @@ int main() {
                 running = false;
             }
             mouse.update(event);
-            button.update(mouse);
         }
         SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
         SDL_RenderClear(renderer);
