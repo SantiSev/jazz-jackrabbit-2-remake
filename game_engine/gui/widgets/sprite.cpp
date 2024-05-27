a07b7d349c95d38bcbd0b363d00c5432fe315d6b
maxogod <maxi18wars@gmail.com>
2024-05-27 18:31:38 -0300

    Creacion de clase Sprite 	- esta renderiza una imagen en pantalla 	- source rect es para recortar una parte de la imagen 	- destination rect es para dibujar en esa parte de la pantalla
    TODO:
    	- transparencia

---
+++ game_engine/gui/widgets/sprite.cpp
@@ -0,0 +1,21 @@
+#include "sprite.h"
+
+Sprite::Sprite(const std::string& file, const SDL_Rect& s_rect, const SDL_Rect& d_rect):
+        s_rect(s_rect), d_rect(d_rect) {
+    surface = IMG_Load(file.c_str());
+    if (surface == nullptr) {
+        throw std::runtime_error("Error loading image");
+    }
+}
+
+void Sprite::draw(SDL_Renderer* renderer) {
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_RenderCopy(renderer, texture, &s_rect, &d_rect);
+    SDL_DestroyTexture(texture);
+}
+
+bool Sprite::is_intersecting(SDL_Point& point) { return SDL_PointInRect(&point, &d_rect); }
+
+bool Sprite::is_intersecting(SDL_Rect& rect) { return SDL_HasIntersection(&d_rect, &rect); }
+
+Sprite::~Sprite() { SDL_FreeSurface(surface); }
---
+++ game_engine/gui/widgets/sprite.h
@@ -0,0 +1,30 @@
+#ifndef TP_FINAL_SPRITE_H
+#define TP_FINAL_SPRITE_H
+
+#include <stdexcept>
+#include <string>
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+#include "../canvas_object.h"
+
+class Sprite: public CanvasObject {
+private:
+    SDL_Rect s_rect;
+    SDL_Rect d_rect;
+    SDL_Surface* surface;
+
+public:
+    Sprite(const std::string& file, const SDL_Rect& s_react, const SDL_Rect& d_rect);
+
+    void draw(SDL_Renderer* renderer) override;
+
+    bool is_intersecting(SDL_Point&) override;
+    bool is_intersecting(SDL_Rect&) override;
+
+    ~Sprite() override;
+};
+
+
+#endif  // TP_FINAL_SPRITE_H
