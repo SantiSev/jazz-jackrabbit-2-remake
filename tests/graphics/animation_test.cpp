77c47a9aa8909beaf9c051867e0937de79d916c6
maxogod <maxi18wars@gmail.com>
2024-05-28 18:34:12 -0300

    Adapto prueba a nuevos cambios e incluyo FPS 	- ahora esta prueba corre a 60fps
    TODO:
    	- ver el tema de dropear frames y catchup

--- tests/graphics/animation_test.cpp
+++ tests/graphics/animation_test.cpp
@@ -10,17 +10,37 @@ int main() {
     SDL_Window* window = nullptr;
     SDL_Renderer* renderer = nullptr;
     SDL_Event event;
-    SDL_Init(SDL_INIT_EVERYTHING);
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+        std::cerr << "Error initializing SDL:" << SDL_GetError() << std::endl;
+        return 1;
+    }
 
     bool running = true;
 
-    SDL_CreateWindowAndRenderer(800, 600, 0, &window, &renderer);
+    if (SDL_CreateWindowAndRenderer(800, 600, 0, &window, &renderer) < 0) {
+        std::cerr << "Error creating window and renderer:" << SDL_GetError() << std::endl;
+        return 1;
+    }
 
-    IMG_Init(IMG_INIT_PNG);
+    if (IMG_Init(IMG_INIT_PNG) == 0) {
+        std::cerr << "Error initializing SDL_image:" << IMG_GetError() << std::endl;
+        return 1;
+    }
+
     AnimatedSprite sprite("/home/maxo/Desktop/taller/assets/jazz.png", {0, 420, 53, 50},
-                          {0, 0, 800, 600}, 13, 1);
+                          {0, 0, 800, 600}, 13, 8, renderer);
+
+    Uint32 frame_start;
+    int frame_time;
+    const int frameDelay = 1000 / 60;
 
     while (running) {
+        // Updates
+        int delta_time = SDL_GetTicks() - frame_start;
+        sprite.update(delta_time);
+
+        frame_start = SDL_GetTicks();
+
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT) {
                 running = false;
@@ -29,10 +49,20 @@ int main() {
         SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
         SDL_RenderClear(renderer);
 
+        // Draw
         sprite.draw(renderer);
-
         SDL_RenderPresent(renderer);
-        SDL_Delay(60);
+
+        frame_time = SDL_GetTicks() - frame_start;
+        if (frameDelay > frame_time) {  // Delay to achieve 60 fps
+            SDL_Delay(frameDelay - frame_time);
+        }
     }
+
+    IMG_Quit();
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
