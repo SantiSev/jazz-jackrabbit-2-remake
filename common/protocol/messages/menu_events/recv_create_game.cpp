0313e9af108c54f688d0f3102198f037eab7a49d
Agustin Barbalase <abarbalase@fi.uba.ar>
2024-05-29 14:49:22 -0300

    Added std::string by reference

--- common/protocol/messages/menu_events/recv_create_game.cpp
+++ common/protocol/messages/menu_events/recv_create_game.cpp
@@ -1,8 +1,10 @@
 #include "./recv_create_game.h"
 
+#include <iostream>
+
 RecvCreateGameMessage::RecvCreateGameMessage(uint16_t id_player, const std::string& match_name) {
-    this->id_player = id_player;
     this->match_name = match_name;
+    this->id_player = id_player;
 }
 
 void RecvCreateGameMessage::run() {}
