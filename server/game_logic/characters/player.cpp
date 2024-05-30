d1fec36b70f088fc23b26dbaf1e55413cf2a1bcc
NIKO <nrsanchez94@fi.uba.ar>
2024-05-30 20:03:58 -0300

    [Game Logic] implementado estados del jugador para luego ser renderizado dependiendo del estado/state, creada logica básica de armas, funciona con 3 armas pero pueden agregarse facilmente mas.

--- server/game_logic/characters/player.cpp
+++ server/game_logic/characters/player.cpp
@@ -1,13 +1,34 @@
 #include "player.h"
 
+#include <cstdint>
 #include <utility>
+#include "../../../common/common_constants.h"
 
 Player::Player(size_t id, std::string name, std::string character):
         id(id),
         name(std::move(name)),
         health(MAX_HEALTH),
         character(std::move(character)),
-        points(STARTING_POINTS) {}
+        points(STARTING_POINTS),
+        state(STATE_IDLE_RIGHT)
+        {
+            set_starting_weapon();
+}
+void Player::set_starting_weapon() {
+    weapons[DEFAULT_WEAPON].set_is_empty(false);
+    weapons[DEFAULT_WEAPON].set_weapon_name(DEFAULT_WEAPON);
+    weapons[DEFAULT_WEAPON].set_ammo(100);
+    weapons[FIRST_WEAPON].set_is_empty(true);
+    weapons[FIRST_WEAPON].set_weapon_name(FIRST_WEAPON);
+    weapons[FIRST_WEAPON].set_ammo(0);
+    weapons[SECOND_WEAPON].set_ammo(0);
+    weapons[SECOND_WEAPON].set_is_empty(true);
+    weapons[SECOND_WEAPON].set_weapon_name(SECOND_WEAPON);
+    weapons[THIRD_WEAPON].set_ammo(0);
+    weapons[THIRD_WEAPON].set_is_empty(true);
+    weapons[THIRD_WEAPON].set_weapon_name(THIRD_WEAPON);
+    weapons[THIRD_WEAPON].set_ammo(0);
+}
 
 size_t Player::get_id() const { return id; }
 
@@ -18,6 +39,8 @@ size_t Player::get_health() const { return health; }
 std::string Player::get_character() { return character; }
 
 size_t Player::get_points() const { return points; }
+
+void Player::add_points(size_t new_points) { this->points += new_points; }
 
 void Player::set_id(const size_t new_id) { this->id = new_id; }
 
@@ -34,7 +57,7 @@ void Player::decrease_health(size_t susbstract_health) {
 }
 
 void Player::increase_health(size_t add_health) {
-    if (this->health > MAX_HEALTH) {
+    if (((int)this->health + (int)add_health) > MAX_HEALTH) {
         this->health = MAX_HEALTH;
     } else {
         this->health += add_health;
@@ -45,6 +68,47 @@ void Player::set_character(std::string new_character) {
     this->character = std::move(new_character);
 }
 
-void Player::set_points(size_t new_points) { this->points = new_points; }
 
 void Player::increase_points(size_t new_points) { this->points += new_points; }
+
+void Player::decrease_revive_cooldown() { this->revive_cooldown--;}
+
+void Player::reset_revive_cooldown() {this->revive_cooldown = REVIVE_COOLDOWN; }
+
+void Player::get_weapon_ammo(size_t ammo, size_t weapon) {
+    this->weapons[weapon].add_ammo(ammo);
+    this->weapons[weapon].set_is_empty(false);
+}
+
+void Player::shoot_selected_weapon(){
+    if (selected_weapon == DEFAULT_WEAPON) {
+        // GENERAR PROYECTIL DEFAULT (no decrementa balas)
+    }
+    if (!weapons[selected_weapon].is_weapon_empty())
+        this->weapons[selected_weapon].decrease_ammo();
+    if (weapons[selected_weapon].get_ammo() == 0){
+        weapons[selected_weapon].set_is_empty(true);
+    }
+    // GENERAR PROYECTIL
+}
+
+
+void Player::select_weapon(size_t weapon_number) {
+    if (!weapons[weapon_number].is_weapon_empty() ) {
+        this->selected_weapon = weapons[weapon_number].get_weapon_name();
+    }
+}
+
+bool Player::can_revive() const{
+    return (!is_alive && revive_cooldown == 0);
+}
+
+void Player::revive(){
+    this->health = MAX_HEALTH;
+    this->revive_cooldown = REVIVE_COOLDOWN;
+    this->state = STATE_IDLE_RIGHT;
+    // posicionar en spawn
+}
+
+uint8_t Player::get_state() const { return state; }
+
