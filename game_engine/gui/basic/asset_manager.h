ce3c979199e497feb733aeeb1ddf96c7ae24f596
maxogod <maxi18wars@gmail.com>
2024-06-02 20:26:12 -0300

    Creacion de asset manager 	- este maneja los assets para poder contruir los paths a partir del working dir

---
+++ game_engine/gui/basic/asset_manager.h
@@ -0,0 +1,21 @@
+#ifndef TP_FINAL_ASSET_MANAGER_H
+#define TP_FINAL_ASSET_MANAGER_H
+
+#include <string>
+#include <SDL2/SDL.h>
+
+#include "../../errors.h"
+
+class AssetManager {
+private:
+    char* base_path;
+
+public:
+    AssetManager();
+
+    std::string get_full_path(const std::string& file) const;
+
+    ~AssetManager();
+};
+
+#endif //TP_FINAL_ASSET_MANAGER_H
