7890c14cc037bae5636b3ff4042dc8c6288b6740
maxogod <maxi18wars@gmail.com>
2024-06-03 15:11:17 -0300

    Creacion de ResourcePool 	- clase para hacer pre-load de los recursos 	- para usarlos compartidos entre widgets

---
+++ game_engine/gui/basic/resource_pool.h
@@ -0,0 +1,40 @@
+#ifndef TP_FINAL_RESOURCE_POOL_H
+#define TP_FINAL_RESOURCE_POOL_H
+
+#include <memory>
+#include <string>
+#include <unordered_map>
+
+#include <SDL2/SDL.h>
+
+#include "asset_manager.h"
+#include "font.h"
+#include "texture.h"
+
+// Uses a path pseudo-relative to the project root directory to load and retrieve resources
+// Example: "assets/images/texture.png"
+class ResourcePool {
+private:
+    SDL_Renderer* renderer;
+    AssetManager asset_manager;
+    std::unordered_map<std::string, std::shared_ptr<Texture>> textures;
+    std::unordered_map<std::string, std::shared_ptr<Font>> fonts;
+
+public:
+    explicit ResourcePool(SDL_Renderer* renderer);
+
+    // cant copy
+    ResourcePool(const ResourcePool&) = delete;
+    ResourcePool& operator=(const ResourcePool&) = delete;
+
+    void load_texture(const std::string& name);
+    void load_font(const std::string& name, int size);
+
+    std::shared_ptr<Texture>& get_texture(const std::string& name);
+    std::shared_ptr<Font>& get_font(const std::string& name);
+
+    ~ResourcePool();
+};
+
+
+#endif  // TP_FINAL_RESOURCE_POOL_H
