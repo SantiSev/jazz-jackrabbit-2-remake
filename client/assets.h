1791d604004bf33943bd72766765ab5352fb5b3a
maxogod <maxi18wars@gmail.com>
2024-06-05 01:56:47 -0300

    Agrego constantes para los assets
    TODO:
    	- no pasa pre-commit pq deberia haber constantes de std::string

---
+++ client/assets.h
@@ -0,0 +1,14 @@
+#ifndef TP_FINAL_ASSETS_H
+#define TP_FINAL_ASSETS_H
+
+#include <string>
+
+// Textures
+const std::string BACKGROUNDS = "assets/screens.png";
+const std::string JAZZ = "assets/jazz.png";
+
+// Fonts
+const std::string FONT = "assets/fonts/atus.ttf";
+const int FONT_SIZE = 20;
+
+#endif  // TP_FINAL_ASSETS_H
