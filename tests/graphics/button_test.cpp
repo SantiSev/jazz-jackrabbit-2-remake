763f0bd851dc64f4c38dbef9dd189042bdc0d222
maxogod <maxi18wars@gmail.com>
2024-06-05 02:01:15 -0300

    Modifico test ResourcePool para adaptase a nuevos cambios y comento test button por ahora

--- tests/graphics/button_test.cpp
+++ tests/graphics/button_test.cpp
@@ -13,67 +13,67 @@
 #include "../../game_engine/gui/widgets/label.h"
 
 int main() {
-    SDL_Window* window = nullptr;
-    SDL_Renderer* renderer = nullptr;
-
-    if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
-        std::cerr << "Error initializing SDL:" << SDL_GetError() << std::endl;
-        return 1;
-    }
-
-    if (SDL_CreateWindowAndRenderer(800, 600, 0, &window, &renderer) < 0) {
-        std::cerr << "Error creating window and renderer:" << SDL_GetError() << std::endl;
-        SDL_Quit();
-        return 1;
-    }
-
-    if (TTF_Init() < 0) {
-        std::cerr << "Error initializing TTF:" << TTF_GetError() << std::endl;
-        SDL_DestroyRenderer(renderer);
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return 1;
-    }
-
-    SDL_Event event;
-    bool running = true;
-    engine::AssetManager asset_manager;
-
-    {
-        auto font = std::make_shared<engine::Font>(
-                asset_manager.get_full_path("assets/fonts/atus.ttf"), 15);
-
-        engine::Mouse mouse(0, 0);
-
-        SDL_Rect rect_label = {300, 200, 100, 100};
-        engine::Label label(font, rect_label, {0, 0, 0, 255}, {255, 255, 255, 255}, "Hello",
-                            renderer);
-
-        SDL_Rect rect = {300, 200, 100, 100};
-        engine::Button button(std::move(label), rect, {255, 255, 255, 255}, {0, 0, 0, 255});
-        mouse.add_on_click_signal_obj(&button);
-
-        while (running) {
-            while (SDL_PollEvent(&event)) {
-                if (event.type == SDL_QUIT) {
-                    running = false;
-                }
-                mouse.update(event);
-            }
-            SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-            SDL_RenderClear(renderer);
-
-            button.draw(renderer);
-
-            SDL_RenderPresent(renderer);
-            SDL_Delay(10);
-        }
-    }
-
-    TTF_Quit();
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    //    SDL_Window* window = nullptr;
+    //    SDL_Renderer* renderer = nullptr;
+    //
+    //    if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+    //        std::cerr << "Error initializing SDL:" << SDL_GetError() << std::endl;
+    //        return 1;
+    //    }
+    //
+    //    if (SDL_CreateWindowAndRenderer(800, 600, 0, &window, &renderer) < 0) {
+    //        std::cerr << "Error creating window and renderer:" << SDL_GetError() << std::endl;
+    //        SDL_Quit();
+    //        return 1;
+    //    }
+    //
+    //    if (TTF_Init() < 0) {
+    //        std::cerr << "Error initializing TTF:" << TTF_GetError() << std::endl;
+    //        SDL_DestroyRenderer(renderer);
+    //        SDL_DestroyWindow(window);
+    //        SDL_Quit();
+    //        return 1;
+    //    }
+    //
+    //    SDL_Event event;
+    //    bool running = true;
+    //    engine::AssetManager asset_manager;
+    //
+    //    {
+    //        auto font = std::make_shared<engine::Font>(
+    //                asset_manager.get_full_path("assets/fonts/atus.ttf"), 15);
+    //
+    //        engine::Mouse mouse(0, 0);
+    //
+    //        SDL_Rect rect_label = {300, 200, 100, 100};
+    //        engine::Label label(font, rect_label, {0, 0, 0, 255}, {255, 255, 255, 255}, "Hello",
+    //                            renderer);
+    //
+    //        SDL_Rect rect = {300, 200, 100, 100};
+    //        engine::Button button(std::move(label), rect, {255, 255, 255, 255}, {0, 0, 0, 255});
+    //        mouse.add_on_click_signal_obj(&button);
+    //
+    //        while (running) {
+    //            while (SDL_PollEvent(&event)) {
+    //                if (event.type == SDL_QUIT) {
+    //                    running = false;
+    //                }
+    //                mouse.update(event);
+    //            }
+    //            SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    //            SDL_RenderClear(renderer);
+    //
+    //            button.draw(renderer);
+    //
+    //            SDL_RenderPresent(renderer);
+    //            SDL_Delay(10);
+    //        }
+    //    }
+    //
+    //    TTF_Quit();
+    //    SDL_DestroyRenderer(renderer);
+    //    SDL_DestroyWindow(window);
+    //    SDL_Quit();
 
     return 0;
 }
