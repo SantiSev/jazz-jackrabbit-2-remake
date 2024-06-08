01e2ecb219ca7faf70f17b3d50e74cf40650851c
nrsanchezfiuba <nrsanchez@fi.uba.ar>
2024-06-08 16:30:00 -0300

    copiado cambios de rama de maxo para pasar bien el git action. en merge descomentar en menu.cpp.

--- client/scenes/menu.cpp
+++ client/scenes/menu.cpp
@@ -20,13 +20,15 @@ MenuScene::MenuScene(engine::Window& window, EventLoop* event_loop,
 
     // Create buttons
     SDL_Rect create_match_button_d_rect = {300, 300, 200, 50};
-    CreateMatchButton* create_match_button = new CreateMatchButton(
-            renderer, resource_pool, create_match_button_d_rect, menu_running, match_running, message_handler);
+    CreateMatchButton* create_match_button =
+            new CreateMatchButton(renderer, resource_pool, create_match_button_d_rect, menu_running,
+                                  match_running, message_handler);
     buttons.push_back(create_match_button);
 
     SDL_Rect join_match_button_d_rect = {300, 370, 200, 50};
-    JoinMatchButton* join_match_button = new JoinMatchButton(
-            renderer, resource_pool, join_match_button_d_rect, menu_running, match_running, message_handler);
+    JoinMatchButton* join_match_button =
+            new JoinMatchButton(renderer, resource_pool, join_match_button_d_rect, menu_running,
+                                match_running, message_handler);
     buttons.push_back(join_match_button);
 
     SDL_Rect quit_button_d_rect = {300, 440, 200, 50};
@@ -36,43 +38,43 @@ MenuScene::MenuScene(engine::Window& window, EventLoop* event_loop,
 }
 
 void MenuScene::start() {
-    // Add buttons to mouse signal of event loop
-    for (auto button: buttons) {
-        event_loop->mouse.add_on_click_signal_obj(button);
-    }
-
-    Uint32 frame_start = 0;
-    Uint32 frame_time = 0;
-    const int frame_delay = 1000 / 60;
-
-    while (menu_running) {
-        // Updates
-        int delta_time = SDL_GetTicks() - frame_start;
-        background->update(delta_time);
-        for (auto button: buttons) {
-            button->update(delta_time);
-        }
-
-        frame_start = SDL_GetTicks();
-
-        // Draw
-        window.clear();
-        background->draw(renderer);
-        for (auto button: buttons) {
-            button->draw(renderer);
-        }
-        window.render();
-
-        frame_time = SDL_GetTicks() - frame_start;
-        if (frame_delay > frame_time) {  // Delay to achieve desired fps
-            SDL_Delay(frame_delay - frame_time);
-        }
-    }
-
-    // Disconnect from mouse signals
-    for (auto button: buttons) {
-        event_loop->mouse.remove_on_click_signal_obj(button);
-    }
+    //    // Add buttons to mouse signal of event loop
+    //    for (auto button: buttons) {
+    //        event_loop->mouse.add_on_click_signal_obj(button);
+    //    }
+    //
+    //    Uint32 frame_start = 0;
+    //    Uint32 frame_time = 0;
+    //    const int frame_delay = 1000 / 60;
+    //
+    //    while (menu_running) {
+    //        // Updates
+    //        int delta_time = SDL_GetTicks() - frame_start;
+    //        background->update(delta_time);
+    //        for (auto button: buttons) {
+    //            button->update(delta_time);
+    //        }
+    //
+    //        frame_start = SDL_GetTicks();
+    //
+    //        // Draw
+    //        window.clear();
+    //        background->draw(renderer);
+    //        for (auto button: buttons) {
+    //            button->draw(renderer);
+    //        }
+    //        window.render();
+    //
+    //        frame_time = SDL_GetTicks() - frame_start;
+    //        if (frame_delay > frame_time) {  // Delay to achieve desired fps
+    //            SDL_Delay(frame_delay - frame_time);
+    //        }
+    //}
+    //
+    //// Disconnect from mouse signals
+    // for (auto button: buttons) {
+    //     event_loop->mouse.remove_on_click_signal_obj(button);
+    // }
 }
 
 MenuScene::~MenuScene() {
