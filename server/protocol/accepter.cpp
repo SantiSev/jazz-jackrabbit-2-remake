5ffc8407d2e800a20cdb313eff922a48bff66b50
maxogod <maxogod>
2024-06-07 23:06:02 -0300

    Minor changes - Server 	- modifico algunas cosas para seguir buenas practicas 	- falta mucho refactor TODO 	- funcionamiento sigue igual, de eso no toque nada

--- server/protocol/accepter.cpp
+++ server/protocol/accepter.cpp
@@ -6,29 +6,28 @@
 #include "../game_logic/matches_manager.h"
 
 
-ServerAccepter::ServerAccepter(const std::string& port):
-        skt(Socket(port.c_str())), online(true), matches_manager() {}
+ServerAccepter::ServerAccepter(const std::string& port): skt(Socket(port.c_str())) {}
 
 void ServerAccepter::run() {
     try {
         matches_manager.start();
-        while (online) {
+        while (_keep_running) {
             Socket peer = skt.accept();
-            if (!online) {
+            if (!_keep_running) {
                 break;
             }
             matches_manager.add_new_client_to_manager(std::move(peer));
         }
     } catch (const std::exception& err) {
-        if (online) {
+        if (_keep_running) {
             std::cerr << "An exception was caught in accepter: " << err.what() << "\n";
         }
     }
 }
 
 void ServerAccepter::stop() {
-    online = false;
-    skt.shutdown(2);
+    _keep_running = false;
+    skt.shutdown(SHUT_RDWR);  // TODO revisar esto
     skt.close();
     matches_manager.stop();
     matches_manager.join();
