5d94ea4b38fdf1c0857e697391e6efc8743c0a6a
Agustín Barbalase <agustinbarbalase@gmail.com>
2024-06-10 16:24:29 -0300

    Added constructors to `Enemies`

--- server/game_logic/characters/enemies.h
+++ server/game_logic/characters/enemies.h
@@ -1,5 +1,11 @@
 #include "enemy.h"
 
-class MadHatter: public Enemy {};
+class MadHatter: public Enemy {
+public:
+    MadHatter(size_t id, int x, int y);
+};
 
-class LizardGoon: public Enemy {};
+class LizardGoon: public Enemy {
+public:
+    LizardGoon(size_t id, int x, int y);
+};
