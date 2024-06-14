f985bc63c1bf55336e562ab928345eddf203c213
maxogod <maxi18wars@gmail.com>
2024-06-13 22:51:27 -0300

    Creacion de nuevo Thread MessageRunner 	- sera usado para desacoplar los mensajes de los inputs de usuario

---
+++ client/protocol/message_runner.h
@@ -0,0 +1,20 @@
+#ifndef MESSAGE_RUNNER_H
+#define MESSAGE_RUNNER_H
+
+#include "../../common/common_thread.h"
+
+#include "client_message_handler.h"
+
+class MessageRunner: public Thread {
+private:
+    ClientMessageHandler& message_handler;
+
+public:
+    Queue<std::shared_ptr<Message>> recv_message;
+
+    explicit MessageRunner(ClientMessageHandler& message_handler);
+    void run() override;
+    ~MessageRunner() override;
+};
+
+#endif
