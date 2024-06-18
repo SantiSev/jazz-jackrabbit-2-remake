ed62664d34605696dfa3b76c611842725cfae30f
santi <santisevitz@hotmail.com>
2024-06-18 10:29:23 -0300

    removing floating enemies

--- server/game_logic/characters/enemies.cpp
+++ server/game_logic/characters/enemies.cpp
@@ -1,6 +1,8 @@
 
 #include "enemies.h"
 
+#define OFFSET(h) ((h)*0.5)
+
 #define MAD_HATTER_ATTACK_DAMAGE 10
 #define MAD_HATTER_HEALTH 100
 #define MAD_HATTER_REVIVE_COOLDOWN 120
@@ -15,8 +17,8 @@
 
 MadHatter::MadHatter(uint16_t id, int x, int y, int w, int h):
         Enemy(id, MAD_HATTER, MAD_HATTER_ATTACK_DAMAGE, MAD_HATTER_HEALTH,
-              MAD_HATTER_REVIVE_COOLDOWN, x, y, w, h, MAD_HATTER_SPEED) {}
+              MAD_HATTER_REVIVE_COOLDOWN, x, y, w, h - OFFSET(h), MAD_HATTER_SPEED) {}
 
 LizardGoon::LizardGoon(uint16_t id, int x, int y, int w, int h):
         Enemy(id, LIZARD_GOON, LIZARD_GOON_ATTACK_DAMAGE, LIZARD_GOON_HEALTH,
-              LIZARD_GOON_REVIVE_COUNTDOWN, x, y, w, h, LIZARD_GOON_SPEED) {}
+              LIZARD_GOON_REVIVE_COUNTDOWN, x, y, w, h - OFFSET(h), LIZARD_GOON_SPEED) {}
