bf3830336e396a18d0ec2b3bbfdca9d7339ca7e6
Agustin Barbalase <abarbalase@fi.uba.ar>
2024-06-18 17:34:32 -0300

    Added `SoundManager` in attr of `Client`

--- client/client.cpp
+++ client/client.cpp
@@ -12,7 +12,8 @@ Client::Client(const std::string& host, const std::string& port):
         event_loop(new EventLoop(game_running, menu_running, match_running, message_handler)),
         message_runner(new MessageRunner(message_handler)),
         thread_manager(new ClientThreadManager(host, port, message_runner->recv_message,
-                                               message_handler.send_message)) {
+                                               message_handler.send_message)),
+        sound_manager(std::make_shared<engine::SoundManager>(resource_pool)) {
     // Pre-load necessary resources
     pre_load_resources(resource_pool);
     message_runner->start();
