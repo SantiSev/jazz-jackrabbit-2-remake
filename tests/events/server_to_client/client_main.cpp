cfd34277780ae9328548f8e736c610e58d9a9193
Agustin Barbalase <abarbalse@fi.uba.ar>
2024-06-24 21:50:39 -0300

    Uncommented `client_to_server/server_main` and `server_to_client/client_main`

--- tests/events/server_to_client/client_main.cpp
+++ tests/events/server_to_client/client_main.cpp
@@ -7,25 +7,25 @@
 #define hostname argv[1]
 #define servname argv[2]
 
-// int main(int argc, const char* argv[]) {
-//     try {
-//         if (argc != 3) {
-//             std::cerr << "Expected format: ./client <hostname> <port>\n";
-//         }
-//
-//         ClientProtocol protocol(hostname, servname);
-//
-//         std::string command;
-//         while (std::cin >> command) {
-//             if (command.compare("q") == 0)
-//                 break;
-//         }
-//     } catch (const std::exception& err) {
-//         std::cerr << "Something went wrong and an exception was caught: " << err.what() << "\n";
-//         return -1;
-//     } catch (...) {
-//         std::cerr << "Something went wrong and an unknown exception was caught.\n";
-//         return -1;
-//     }
-//     return 0;
-// }
+int main(int argc, const char* argv[]) {
+    try {
+        if (argc != 3) {
+            std::cerr << "Expected format: ./client <hostname> <port>\n";
+        }
+
+        ClientProtocol protocol(hostname, servname);
+
+        std::string command;
+        while (std::cin >> command) {
+            if (command.compare("q") == 0)
+                break;
+        }
+    } catch (const std::exception& err) {
+        std::cerr << "Something went wrong and an exception was caught: " << err.what() << "\n";
+        return -1;
+    } catch (...) {
+        std::cerr << "Something went wrong and an unknown exception was caught.\n";
+        return -1;
+    }
+    return 0;
+}
